b2e0cc2b65c28a1406dfa01ad2753efb4b430d05
Quinn Shim <quinnshim@Quinns-MacBook-Pro.local>
2020-05-14 03:46:45 -0400

    test small change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Some simple changes for a test
         </a>
       </header>
     </div>
