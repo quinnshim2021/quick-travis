7ba6b785cb7bb7d1b56e2939eb358985bafc9628
Quinn Shim <quinnshim@Quinns-MacBook-Pro.local>
2020-05-15 01:02:32 -0400

    change to see if deployed

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Quick-travis!!
         </a>
       </header>
     </div>
